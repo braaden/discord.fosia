68ae27ed7358750f57f65af960313e01fa0a8313
Braden <bradendiscord@gmail.com>
2018-04-27 17:45:51 -0500

    Create fosia.cc

---
+++ fosia.cc
@@ -0,0 +1,9 @@
+int main()
+{
+  std::cout << "fosia";
+  std::cout << "This is a program for you to edit and code with!";
+  std::cout << "We're really still working on this!";
+  std::cout << "WEBSITE SOON";
+  std::cout << "Copyright (c) Braaden";
+
+}
